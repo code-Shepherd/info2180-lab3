5e309881e7dfb3350b9f3bc9b1837713d2e27c64
code-Shepherd <64187337+code-Shepherd@users.noreply.github.com>
2023-10-22 18:43:03 -0500

    Add files via upload
    exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -68,4 +68,68 @@ document.addEventListener('DOMContentLoaded', function () {
       return false;
     }
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';
+  
+    const gameState = ['', '', '', '', '', '', '', '', ''];
+  
+    function handleSquareClick(event) {
+      const square = event.target;
+      const squareIndex = Array.from(squares).indexOf(square);
+  
+      if (gameState[squareIndex] === '' && !checkGameWin()) {
+        gameState[squareIndex] = currentPlayer;
+        square.classList.add(currentPlayer);
+        square.textContent = currentPlayer;
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  
+        if (checkGameWin()) {
+          document.getElementById('status').textContent = `Player ${currentPlayer} wins! 🎉`;
+          document.getElementById('status').classList.add('you-won');
+        }
+      }
+    }
+  
+    function handleSquareMouseOver(event) {
+      const square = event.target;
+      // Apply the 'hover' class when the mouse is over the square
+      square.classList.add('hover');
+    }
+  
+    function handleSquareMouseLeave(event) {
+      const square = event.target;
+      // Remove the 'hover' class when the mouse leaves the square
+      square.classList.remove('hover');
+    }
+  
+    squares.forEach((square) => {
+      square.addEventListener('click', handleSquareClick);
+      square.addEventListener('mouseover', handleSquareMouseOver);
+      square.addEventListener('mouseleave', handleSquareMouseLeave);
+    });
+  
+    function checkGameWin() {
+      const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+  
+      for (const combo of winCombos) {
+        const [a, b, c] = combo;
+        if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+          return true;
+        }
+      }
+  
+      return false;
+    }
+  });
   
